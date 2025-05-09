21891dd2e9bc8d617179b50435aa8cae2c231daf
chengang.wang <wangchengang@i-click.com>
2025-05-09 16:01:42 +0800

    Initial Commit

---
+++ NasTool/NasToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasToolApp.swift
+//  NasTool
+//
+//  Created by chengang.wang on 2025/5/9.
+//
+
+import SwiftUI
+
+@main
+struct NasToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NasToolTests/NasToolTests.swift
@@ -0,0 +1,17 @@
+//
+//  NasToolTests.swift
+//  NasToolTests
+//
+//  Created by chengang.wang on 2025/5/9.
+//
+
+import Testing
+@testable import NasTool
+
+struct NasToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
